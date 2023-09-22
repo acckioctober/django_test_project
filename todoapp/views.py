a3c0996bb32a8f3860b708a71e6a4918643de3a7
acckioctober <acckioctober@gmail.com>
2023-09-22 17:52:12 +0600

    created simple todo list

---
+++ todoapp/views.py
@@ -0,0 +1,24 @@
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .models import Task
+from .serializers import TaskSerializer
+
+class TaskListCreateView(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
